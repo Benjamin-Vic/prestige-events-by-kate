26bef1ee902797d3cc23f344e3709ace8fed74bc
Benjamin Vic <71839584+Benjamin-Vic@users.noreply.github.com>
2022-06-21 02:49:46 +0200

    first push

---
+++ src/App.svelte
@@ -0,0 +1,120 @@
+<script>
+  import Slider from "./components/slider/Slider.svelte";
+</script>
+
+<main>
+  <div class="border border-head">
+    <span class="border-head-text">
+      Kate, Lieu-dit Le Mas Blanc 66200 ALENYA
+    </span>
+
+    <span class="border-head-text"> Téléphone : 06 89 71 52 21 </span>
+  </div>
+
+  <div class="border border-left" />
+  <div class="border border-right" />
+  <div class="border border-foot" />
+
+  <Slider />
+
+  <span>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nulla id
+    tellus tincidunt tempus in et lectus. Sed hendrerit metus quam. Donec
+    tempor, nulla sed scelerisque eleifend, diam eros elementum tortor, nec
+    venenatis nibh justo ut metus. Donec non turpis id lorem sagittis fringilla
+    porttitor id lectus. Curabitur ornare placerat mauris, in venenatis justo.
+    Phasellus volutpat sollicitudin risus ornare pellentesque. Mauris laoreet
+    nulla sit amet scelerisque viverra. Integer orci lacus, sollicitudin vel
+    imperdiet vel, lacinia nec ligula. Nulla vel velit id mauris fermentum
+    sodales imperdiet at tortor. Sed ornare leo eu est accumsan porta. Sed a
+    arcu id tellus commodo dignissim. Phasellus at quam molestie, ullamcorper
+    dolor vitae, imperdiet ligula. Curabitur commodo est vel semper varius.
+    Morbi nibh metus, luctus et suscipit a, tempus a mauris. Aenean velit nisi,
+    lobortis sit amet ante in, venenatis vestibulum dui. Nulla facilisi.
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+    cubilia curae; Maecenas maximus pellentesque ligula quis gravida. Phasellus
+    maximus arcu vel lectus mollis pellentesque. Maecenas tincidunt eu nisl eget
+    dictum. Donec non diam volutpat, laoreet velit et, malesuada lorem.
+    Phasellus vulputate libero velit, ac efficitur magna elementum at.
+    Vestibulum eros leo, accumsan eget rutrum nec, fermentum eu metus. Aenean
+    mauris velit, faucibus eu elit a, egestas vestibulum tellus. Vivamus
+    placerat nibh in erat pulvinar varius. In quis hendrerit urna. Proin
+    ultricies tortor eu tortor sagittis hendrerit. Praesent faucibus urna ac
+    condimentum rutrum. Ut lobortis lorem eget lectus consectetur, ac blandit
+    tellus tempus. Morbi id aliquet orci, eget consectetur nunc. Vivamus euismod
+    odio ut lectus tempus pretium. Phasellus id ex eget odio lobortis gravida.
+    Integer ultricies vitae lacus eu pulvinar. Donec efficitur blandit dolor sed
+    sagittis. Aliquam dui magna, maximus ut lectus a, interdum tincidunt enim.
+    Nam urna sem, venenatis ut ex malesuada, posuere ultricies ligula. Quisque
+    nunc tellus, imperdiet scelerisque tristique aliquet, tristique et nisi. Nam
+    faucibus lacinia ligula, quis elementum tortor feugiat eget. Nam velit
+    augue, pharetra a leo ac, porta sodales ex. Fusce cursus pulvinar elit
+    vulputate pretium. Fusce quis risus a sapien laoreet pulvinar ut at sem.
+    Curabitur sodales, lorem mattis dapibus vulputate, nisl mauris fermentum
+    enim, vitae finibus justo sapien scelerisque mauris. Integer non tellus
+    consequat, cursus augue id, cursus velit.
+  </span>
+</main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap");
+
+  main {
+    width: calc(100% - 64px);
+    height: 100vh;
+
+    border: 32px solid white;
+  }
+
+  .border-head-text {
+    font-family: "Roboto Slab", serif;
+    font-size: small;
+    padding: 0 32px;
+  }
+
+  .border {
+    background-color: rgb(204, 144, 144);
+  }
+
+  .border-head,
+  .border-foot {
+    width: 100%;
+    height: 32px;
+    z-index: 2;
+  }
+
+  .border-left,
+  .border-right {
+    width: 32px;
+    height: 100%;
+    z-index: 1;
+  }
+
+  .border-head {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .border-left {
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+
+  .border-right {
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
+
+  .border-foot {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+  }
+</style>
---
+++ temp/App copy.svelte
@@ -0,0 +1,163 @@
+<script>
+  import logo from "./assets/svelte.png";
+  import Counter from "./src/src/lib/Counter.svelte";
+</script>
+
+<main>
+  <div class="border-container">
+    <div class="border-top">
+      <span class="border-top-text">
+        Kate, Lieu-dit Le Mas Blanc 66200 ALENYA
+      </span>
+
+      <span class="border-top-text"> Téléphone : 06 89 71 52 21 </span>
+    </div>
+
+    <div class="border-body">
+      <div class="border-left" />
+
+
+      <div class="border-right" />
+    </div>
+
+    <div class="border-bottom" />
+  </div>
+
+  <span>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id placerat
+    leo. Mauris sem metus, congue in lobortis sit amet, faucibus a tortor.
+    Nullam vitae lectus vel dui condimentum facilisis sed ac ex. Donec posuere,
+    nunc ut interdum gravida, dolor justo ornare nisi, vel finibus neque risus
+    quis erat. Morbi a aliquet lectus, ac tincidunt diam. Integer blandit vel
+    sem in lobortis. Suspendisse fringilla turpis eget sodales commodo. Nunc non
+    leo scelerisque eros congue dictum. Donec id sem et neque pretium auctor a
+    in neque. Cras et vehicula dui. Vestibulum sit amet eros id leo imperdiet
+    tristique. Integer sed erat ac magna bibendum malesuada. Vivamus sed justo
+    non tellus consectetur fermentum. Etiam fermentum elit in ullamcorper
+    consequat. Phasellus volutpat sollicitudin nulla a lacinia. Nulla tincidunt
+    ultricies sapien eget tempus. Aliquam eu metus non diam ultricies
+    consectetur. Fusce gravida dapibus sapien, ut tempus eros rutrum vitae. Nam
+    volutpat laoreet est quis commodo.
+  </span>
+
+  <!-- <img src={logo} alt="Svelte Logo" />
+  <h1>Hello world!</h1>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p> -->
+</main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap");
+
+  .border-container {
+    position: absolute;
+    width: 100%;
+  }
+
+  .border-top {
+    /* width: 100vw; */
+    height: 32px;
+    background-color: white;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 0 32px;
+
+    /* border: 1px solid red; */
+  }
+
+  .border-top-text {
+    font-family: "Roboto Slab", serif;
+    font-size: small;
+  }
+
+  .border-body {
+    display: flex;
+    justify-content: space-between; 
+  }
+
+  .border-left {
+    width: 32px;
+    height: calc(100vh - 64px);
+    background-color: white;
+
+    /* border: 1px solid green; */
+  }
+
+  .border-right {
+    width: 32px;
+    height: calc(100vh - 64px);
+    background-color: white;
+
+    /* border: 1px solid blue; */
+  }
+
+  .border-bottom {
+    width: 100%;
+    height: 32px;
+    background-color: white;
+
+    /* border: 1px solid red; */
+  }
+
+  .view {
+    width: 100%;
+    height: calc(100vh - 64px);
+
+    /* border: 1px solid violet; */
+  }
+
+  /* :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  } */
+</style>
